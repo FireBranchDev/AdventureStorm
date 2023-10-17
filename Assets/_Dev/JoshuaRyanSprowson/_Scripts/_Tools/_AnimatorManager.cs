db9e7f9c2403aa7e2718fb0e6ff8d2e14237705f
Joshua Ryan-Sprowson <dev.joshuars@gmail.com>
2023-10-18 06:34:30 +0800

    Forgot to add the class namespace

--- Assets/_Dev/JoshuaRyanSprowson/_Scripts/_Tools/_AnimatorManager.cs
+++ Assets/_Dev/JoshuaRyanSprowson/_Scripts/_Tools/_AnimatorManager.cs
@@ -1,43 +1,46 @@
 using UnityEngine;
 
-/// <summary>
-/// In charge of making sure the animations are being played
-/// </summary>
-public class _AnimatorManager : MonoBehaviour
+namespace AdventureStorm
 {
-    #region Fields
-    
-    private Animator _animator;
-    private string _currentState;
-
-    #endregion
-
-    #region LifeCycle
-
-    private void Start()
-    {
-        _animator = GetComponent<Animator>();
-    }
-
-    #endregion
-
-    #region Public Methods
-
     /// <summary>
-    /// Handles changing animation state so that it can be played.
+    /// In charge of making sure the animations are being played
     /// </summary>
-    /// <param name="newState">The new state to play next.</param>
-    public void ChangeAnimationState(string newState)
+    public class _AnimatorManager : MonoBehaviour
     {
-        // Stop the same animation from interrupting itself.
-        if (_currentState == newState) return;
+        #region Fields
 
-        // Play the animation.
-        _animator.Play(newState);
+        private Animator _animator;
+        private string _currentState;
 
-        // Reassign the current state.
-        _currentState = newState;
+        #endregion
+
+        #region LifeCycle
+
+        private void Start()
+        {
+            _animator = GetComponent<Animator>();
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Handles changing animation state so that it can be played.
+        /// </summary>
+        /// <param name="newState">The new state to play next.</param>
+        public void ChangeAnimationState(string newState)
+        {
+            // Stop the same animation from interrupting itself.
+            if (_currentState == newState) return;
+
+            // Play the animation.
+            _animator.Play(newState);
+
+            // Reassign the current state.
+            _currentState = newState;
+        }
+
+        #endregion
     }
-
-    #endregion
 }
