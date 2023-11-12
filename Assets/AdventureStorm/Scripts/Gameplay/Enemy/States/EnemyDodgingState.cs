97a1105deb0dd8f7143e37c8534b858316d06dde
Joshua Ryan-Sprowson <dev.joshuars@gmail.com>
2023-11-12 17:43:26 +0800

    Improved the enemy's dodging mechanics

--- Assets/AdventureStorm/Scripts/Gameplay/Enemy/States/EnemyDodgingState.cs
+++ Assets/AdventureStorm/Scripts/Gameplay/Enemy/States/EnemyDodgingState.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace AdventureStorm.Gameplay
 {
@@ -8,6 +9,12 @@ namespace AdventureStorm.Gameplay
         #region Constant Fields
 
         private const string StartJumpAnimation = "Start Jump";
+        private const string IdleAnimation = "Idle";
+
+        private const float DodgeDistance = 1.35f;
+        private const float DodgeHeight = 0.75f;
+        private const float InAirDuration = 0.2f;
+        private const float DodgeCooldown = 0.5f;
 
         #endregion
 
@@ -21,7 +28,6 @@ namespace AdventureStorm.Gameplay
 
         public override void EnterState(EnemyStateManager enemy)
         {
-            enemy.AnimatorManager.ChangeAnimationState(StartJumpAnimation);
             _dodgeCoroutine = enemy.StartCoroutine(DodgeCoroutine(enemy));
         }
 
@@ -50,7 +56,35 @@ namespace AdventureStorm.Gameplay
 
         private IEnumerator DodgeCoroutine(EnemyStateManager enemy)
         {
-            yield return new WaitForSeconds(0.25f);
+            enemy.AnimatorManager.ChangeAnimationState(StartJumpAnimation);
+
+            Vector3 position = Vector3.zero;
+
+            if (enemy.AliveState.IsFacingLeft)
+            {
+                position.x = DodgeDistance / 2f;
+            }
+            else
+            {
+                position.x = -DodgeDistance / 2f;
+            }
+
+            position.y = DodgeHeight / 2f;
+            enemy.transform.Translate(position);
+            yield return new WaitForSeconds(0.05f);
+            enemy.transform.Translate(position);
+
+            yield return new WaitForSeconds(InAirDuration);
+
+            position.x = 0;
+            position.y = -DodgeHeight;
+
+            enemy.transform.Translate(position);
+
+            enemy.AnimatorManager.ChangeAnimationState(IdleAnimation);
+
+            yield return new WaitForSeconds(DodgeCooldown);
+
             enemy.SwitchState(enemy.AliveState.CombatState);
         }
 
